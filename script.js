ea3adfc8a7598c32e0d38a18e446c48a4d7655d1
avege <angela.vegega@gmail.com>
2021-11-04 21:08:04 +0900

    prompt and consolelog working, about to add score

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 'use strict';
+const playerSelection = '';//won't prompt if not here
+const computerSelection = computerPlay();
+
+let playerScore = 0;
+let compScore = 0;
 
 //computer decides which to throw based on random numbers
 function computerPlay() {
-    const randomNumber = Math.floor(Math.random() * 9) + 1;
-    if (randomNumber >= 1 && randomNumber <= 3) {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 1) {
         return 'Rock';
-    } else if (randomNumber >= 4 && randomNumber <= 6){
+    } else if (randomNumber === 2){
         return 'Paper';
     } else {
         return 'Scissors';
@@ -19,16 +24,22 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'Rock' || playerSelection === 'paper' && computerSelection === 'Paper' || playerSelection === 'scissors' && computerSelection === 'Scissors') {
         return 'It\'s a tie!';
     } else if (playerSelection === 'rock' && computerSelection === 'Paper') {
+        compScore++;
         return 'You lose! Paper beats rock!'
     } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+        playerScore++;
         return 'You win! Rock beats scissors!'
     } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+        playerScore++;
         return 'You win! Paper beats rock!'
     } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
+        compScore++;
         return 'You lose! Scissors beats paper!'
     } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
+        compScore++;
         return 'You lose! Rock beats scissors!'
     } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+        playerScore++;
         return 'You win! Scissors beats paper!'
     } else {
         return 'Please choose rock, paper, or scissors'
@@ -36,8 +47,7 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-const playerSelection = '';//won't prompt if not here
-const computerSelection = computerPlay();
+
 // console.log(playRound(playerSelection, computerSelection));
 function game() {
    
